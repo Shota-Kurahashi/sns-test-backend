6293a3526cef072729ce9a25efc6dae3f8384eb3
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-07-06 19:00:18 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # node-ts
+
+# cd node-prisma
+
+# docker compose up -d
+
+# npm run dev
+
+# npx prisma migrate dev --name init
+
+# npx prisma studio
+
+# npx prisma db seed
+
+# npx prisma migrate reset
+
+# npx prisma migrate reset
+
+# npx prisma db pull
--- server.ts
+++ server.ts
@@ -1,7 +1,29 @@
+import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
+
+const prisma = new PrismaClient();
+app.use(express.json());
+
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+
+  return res.json(users);
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+
+  return res.json(user);
+});
 
 app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
 
